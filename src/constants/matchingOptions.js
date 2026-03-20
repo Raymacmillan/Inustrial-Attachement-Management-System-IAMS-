7202c84a9b41a44b1fb18ab9292944e892b917b0
Ryodevv <raymcmillangumbo@gmail.com>
2026-03-20 19:52:48 +0200

    chore: extract matching options to global constants

---
+++ src/constants/matchingOptions.js
@@ -0,0 +1,31 @@
+export const SUGGESTED_ROLES = [
+  "Frontend Developer", 
+  "Backend Developer", 
+  "Fullstack Engineer",
+  "UI/UX Designer", 
+  "Data Analyst", 
+  "Cybersecurity Analyst", 
+  "GIS Specialist",
+  "Mobile App Developer", 
+  "Machine Learning Engineer", 
+  "Network Administrator"
+];
+
+export const SUGGESTED_SKILLS = [
+  "React", "Node.js", "Python", "SQL", "PostgreSQL", "JavaScript", 
+  "TypeScript", "Java", "C++", "Figma", "AWS", "Git", "Docker"
+];
+
+export const BOTSWANA_LOCATIONS = [
+  'Gaborone', 'Francistown', 'Maun', 'Palapye', 'Selebi-Phikwe', 
+  'Lobatse', 'Molepolole', 'Serowe', 'Kanye', 'Ghanzi', 
+  'Kasane', 'Jwaneng', 'Letlhakane', 'Tlokweng', 'Mogoditshane',
+  'Remote', 'Hybrid'
+];
+
+export const UB_MAJORS = [
+  "B.Sc. (Computer Science)",
+  "B.Sc. (Comp. with Finance)",
+  "B.Sc. (Information Systems)",
+  "B.Sc. (Information Technology)",
+];
