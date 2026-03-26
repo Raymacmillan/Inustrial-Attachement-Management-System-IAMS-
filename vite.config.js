6df2a78575d921ee0d98cfd69ffb38a88959fe95
kaobyte <kaonyenye126@gmail.com>
2026-03-26 12:40:30 +0200

    fix: add confirmation password

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-<<<<<<< Updated upstream
   server: {
     hmr: {
       overlay: false,
     },
   },
 });
-=======
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
->>>>>>> Stashed changes
